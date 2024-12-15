e895dfd2974f8da6c1dfcd855b365baa794f8843
Patrick Leduc <patrick.leduc13@gmail.com>
2024-12-14 19:43:13 -0500

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+variable "ssh_private_key" {
+  type        = string
+  description = "SSH private key for Docker provider"
+}
+
 terraform {
   cloud {
     organization = "patrick-leduc-aws"
@@ -9,13 +14,18 @@ terraform {
 }
 
 provider "docker" {
-  host        = "ssh://pi@192.168.86.60"
-  private_key = file("/path/to/your/private/key")
+  host = "ssh://patrick@173.206.63.235:2222" # SSH connection to your remote server
+  # You can also specify a private key if needed, or it will use the default key
+  private_key = file("~/.ssh/id_rsa")
 }
 
-resource "docker_container" "nginx" {
-  image = "nginx:latest"
-  name  = "nginx-container"
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx_container"
+  image = docker_image.nginx.latest
   ports {
     internal = 80
     external = 8080
