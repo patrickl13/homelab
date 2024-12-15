f1e210b309a534a9c3236a053ec6e1d093356de0
Patrick Leduc <patrick.leduc13@gmail.com>
2024-12-15 10:51:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,4 +18,9 @@ resource "docker_image" "nginx" {
 resource "docker_container" "nginx_container" {
   name  = "nginx_container"
   image = docker_image.nginx.image_id
+
+  ports {
+    internal = 80
+    external = 8080
+  }
 }
