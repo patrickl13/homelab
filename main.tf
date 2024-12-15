8c8733a7fa1981a48190b1f2ee417232da1a6e4d
Patrick Leduc <patrick.leduc13@gmail.com>
2024-12-14 20:29:43 -0500

    ssh

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-variable "ssh_private_key" {
-  type        = string
-  description = "SSH private key for Docker provider"
-}
-
 terraform {
   required_providers {
     docker = {
@@ -22,7 +17,6 @@ terraform {
 provider "docker" {
   host = "ssh://patrick@173.206.63.235:2222" # SSH connection to your remote server
   ssh_opts = [
-    "-i", "/tmp/ssh_private_key",
     "-o", "StrictHostKeyChecking=no",
     "-o", "UserKnownHostsFile=/dev/null"
   ]
