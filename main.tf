8570f4496e2487fe4073ad39ef865b0fe1294464
Patrick Leduc <patrick.leduc13@gmail.com>
2024-12-16 10:22:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -92,3 +92,45 @@ resource "docker_container" "pihole" {
 
   restart = "always"
 }
+
+resource "docker_image" "prometheus" {
+  name = "prom/prometheus:latest"
+}
+
+resource "docker_container" "prometheus" {
+  name  = "prometheus"
+  image = docker_image.prometheus.image_id
+
+  ports {
+    internal = 9090
+    external = 9090
+  }
+
+  volumes {
+    host_path      = "/data/prometheus"
+    container_path = "/prometheus"
+  }
+
+  restart = "always"
+}
+
+resource "docker_image" "grafana" {
+  name = "grafana/grafana:latest"
+}
+
+resource "docker_container" "grafana" {
+  name  = "grafana"
+  image = docker_image.grafana.image_id
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+
+  volumes {
+    host_path      = "/data/grafana"
+    container_path = "/var/lib/grafana"
+  }
+
+  restart = "always"
+}
