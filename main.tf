ddc7f1e3f7b09bec3ac9ffc694fb6d9226b3635d
Patrick Leduc <patrick.leduc13@gmail.com>
2024-12-26 10:44:28 -0500

    drive formating

--- main.tf
+++ main.tf
@@ -162,3 +162,85 @@ resource "docker_container" "grafana" {
     EOT
   }
 }
+
+resource "docker_image" "plex" {
+  name = "linuxserver/plex:latest"
+}
+
+resource "docker_container" "plex" {
+  name  = "plex"
+  image = docker_image.plex.image_id
+
+  ports {
+    internal = 32400
+    external = 32400
+  }
+
+  ports {
+    internal = 32469
+    external = 32469
+    protocol = "udp"
+  }
+
+  ports {
+    internal = 32410
+    external = 32410
+    protocol = "udp"
+  }
+
+  ports {
+    internal = 32412
+    external = 32412
+    protocol = "udp"
+  }
+
+  ports {
+    internal = 32413
+    external = 32413
+    protocol = "udp"
+  }
+
+  ports {
+    internal = 32414
+    external = 32414
+    protocol = "udp"
+  }
+
+  ports {
+    internal = 1900
+    external = 1900
+    protocol = "udp"
+  }
+
+  volumes {
+    host_path      = "/data/plex/config"
+    container_path = "/config"
+  }
+
+  volumes {
+    host_path      = "/data/plex/transcode"
+    container_path = "/transcode"
+  }
+
+  volumes {
+    host_path      = "/mnt/external_drive/movies"
+    container_path = "/movies"
+  }
+
+  environment = {
+    PUID = "1000"
+    PGID = "1003"
+    TZ   = "America/New_York" # Replace with your timezone
+  }
+
+  restart = "always"
+
+  # Ensure the host directories have the correct permissions
+  provisioner "local-exec" {
+    command = <<EOT
+      mkdir -p /data/plex/config /data/plex/transcode /mnt/external_drive/movies && \
+      sudo chown -R 1000:1000 /data/plex /mnt/external_drive/movies && \
+      sudo chmod -R 775 /data/plex /mnt/external_drive/movies
+    EOT
+  }
+}
